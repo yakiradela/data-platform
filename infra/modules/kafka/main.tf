ce5ef163fea67834f0d62b66a09278eba5ebe932
Yakir Adela <yakirade@gmail.com>
2025-09-04 16:37:38 +0300

    Update main.tf

--- infra/modules/kafka/main.tf
+++ infra/modules/kafka/main.tf
@@ -1,11 +1,11 @@
 resource "aws_msk_cluster" "this" {
-    cluster_name            = "${var.env}-msk"
+    cluster_name            = "msk-cluster"
     kafka_version           = "3.7.0"
     number_of_broker_nodes  = 2
 
     broker_node_group_info {
       instance_type         = "kafka.m5.large"
-      client_subnets        = var.private_subnets
+      client_subnets        = "subnet-data"
       security_groups       = [var.sg_id]
     }
 }
