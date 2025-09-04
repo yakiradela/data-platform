985ef2b0727fdf59902638ac0d93be2cb38022f2
Yakir Adela <yakirade@gmail.com>
2025-09-04 17:26:42 +0300

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -4,14 +4,14 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = "data-platform-vpc"
+    Name = "vpc-data-platform"
   }
 }
 
 resource "aws_subnet" "subnet_data" {
-  vpc_id            = "vpc-data-platform"
-  cidr_block        = "10.10.1.0/24"
-  availability_zone = "us-east-2a"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.1.0/24"
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name = "subnet-data"
@@ -19,9 +19,9 @@ resource "aws_subnet" "subnet_data" {
 }
 
 resource "aws_subnet" "subnet_platform" {
-  vpc_id            = "vpc-data-platform"
-  cidr_block        = "10.10.2.0/24"
-  availability_zone = "us-east-2b"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.10.2.0/24"
+  availability_zone       = "us-east-2b"
 
   tags = {
     Name = "subnet-platform"
