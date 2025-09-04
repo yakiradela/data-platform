503cff268ebd7a1e86b52233440bc8eb48d368a0
Yakir Adela <yakirade@gmail.com>
2025-09-04 21:04:14 +0300

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,35 +1,3 @@
-# העתק את ה־VPC וה־subnets מהמודול vpc לכאן
-
-resource "aws_vpc" "main" {
-  cidr_block           = "10.10.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "vpc-data-platform"
-  }
-}
-
-resource "aws_subnet" "subnet_data" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.10.1.0/24"
-  availability_zone = "us-east-2a"
-
-  tags = {
-    Name = "subnet-data"
-  }
-}
-
-resource "aws_subnet" "subnet_platform" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.10.2.0/24"
-  availability_zone = "us-east-2b"
-
-  tags = {
-    Name = "subnet-platform"
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -52,6 +20,17 @@ module "eks" {
       min_size     = 2
       max_size     = 3
       desired_size = 2
+    }
+  }
+
+  node_security_group_additional_rules = {
+    allow_all_vpc_ingress = {
+      description = "Allow all traffic within VPC"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      cidr_blocks = ["10.10.0.0/16"]
     }
   }
 
