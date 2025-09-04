142f2feb50919d1224cb219c1252b91eccf79f9e
Yakir Adela <yakirade@gmail.com>
2025-09-04 16:17:43 +0300

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "subnet_data" {
 }
 
 resource "aws_subnet" "subnet_platform" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = vpc-data-platform
   cidr_block        = "10.10.2.0/24"
   availability_zone = "us-east-2b"
 
