a8ab59f58b07b2d1f8229fac29718874d8579600
Yakir Adela <yakirade@gmail.com>
2025-09-04 19:57:49 +0300

    Update main.tf

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,3 +1,33 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-data-platform"
+  }
+}
+
+resource "aws_subnet" "subnet_data" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "subnet-data"
+  }
+}
+
+resource "aws_subnet" "subnet_platform" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "subnet-platform"
+  }
+}
+
 resource "aws_redshift_subnet_group" "default" {
   name        = "dev-redshift-subnet-group"
   description = "Subnet group for Redshift cluster"
@@ -29,3 +59,4 @@ resource "aws_redshift_cluster" "main" {
     Name        = "dev-redshift"
   }
 }
+
