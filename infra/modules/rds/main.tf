db512ff0183a4723d093e1ee005e296c6ceacf91
Yakir Adela <yakirade@gmail.com>
2025-09-04 17:29:21 +0300

    Update main.tf

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,29 +1,31 @@
-
 resource "aws_redshift_subnet_group" "default" {
   name        = "dev-redshift-subnet-group"
   description = "Subnet group for Redshift cluster"
-  subnet_ids  = ["subnet-data","subnet-platform"]
+  subnet_ids  = [
+    aws_subnet.subnet_data.id,
+    aws_subnet.subnet_platform.id
+  ]
 
   tags = {
     Environment = "dev"
-    Name = "dev-redshift-subnet-group"
+    Name        = "dev-redshift-subnet-group"
   }
 }
 
 resource "aws_redshift_cluster" "main" {
   cluster_identifier         = "dev-redshift"
-  node_type                  = "dc2.large"
+  node_type                  = "ra3.xlplus"
   master_username            = "adminuser"
-  master_password            = "YakiradelaA1"
+  master_password            = "StrongPass123!"
   database_name              = "dataplatform"
   cluster_type               = "single-node"
   port                       = 5439
   publicly_accessible        = false
   skip_final_snapshot        = true
-  cluster_subnet_group_name  = "subnet-data"
+  cluster_subnet_group_name  = aws_redshift_subnet_group.default.name
 
   tags = {
     Environment = "dev"
-    Name = "dev-redshift"
+    Name        = "dev-redshift"
   }
 }
