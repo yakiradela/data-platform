124adf38f2491bb476a14965e855198ba93233d2
Yakir Adela <yakirade@gmail.com>
2025-09-04 19:56:51 +0300

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,3 +1,35 @@
+# העתק את ה־VPC וה־subnets מהמודול vpc לכאן
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-data-platform"
+  }
+}
+
+resource "aws_subnet" "subnet_data" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "subnet-data"
+  }
+}
+
+resource "aws_subnet" "subnet_platform" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "subnet-platform"
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -28,4 +60,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
